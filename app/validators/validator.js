e12badf18403ae08f436f570030a323cd4ae3a50
Arslan <1041238415@qq.com>
2020-05-26 14:12:28 +0800

    Dockerfile

--- app/validators/validator.js
+++ app/validators/validator.js
@@ -19,11 +19,11 @@ class RegisterValidator extends LinValidator {
           min: 6,
           max: 32,
         }),
-        new Rule(
-          "matches",
-          "密码不符合规范",
-          "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]"
-        ),
+        // new Rule(
+        //   "matches",
+        //   "密码不符合规范",
+        //   "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]"
+        // ),
       ]),
       (this.password2 = this.password1),
       (this.nickname = [
@@ -165,45 +165,22 @@ class LoginValidator extends LinValidator {
           min: 6,
           max: 32,
         }),
-        new Rule(
-          "matches",
-          "密码不符合规范",
-          "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]"
-        ),
-      ]),
-      (this.nickname = [
-        new Rule("isLength", "昵称不符合规范", {
-          min: 4,
-          max: 32,
-        }),
       ]);
   }
+}
 
-  validatePassword(vals) {
-    const psw1 = vals.body.password1;
-    const psw2 = vals.body.password2;
-    if (psw1 !== psw2) {
-      throw new Error("输入的密码不一致");
-    }
-  }
-
-  async validateEmail(vals) {
-    const email = vals.body.email;
-    //如果查找相同的email则抛出异常
-    const user = await User.findOne({
-      where: {
-        email: email,
-      },
-    });
-    if (user) {
-      throw new Error("email不能重复注册");
-    }
+class getUserListValidator extends LinValidator {
+  constructor() {
+    super();
+    (this.page = [new Rule("isInt", "page是整数且不能为空")]),
+      (this.size = [new Rule("isInt", "size是整数且不能为空")]);
   }
 }
 
 module.exports = {
   PositiveIntegerValidator,
   RegisterValidator,
+  LoginValidator,
   TokenValidator,
   NotEmptyValidator,
   LikeValidator,
@@ -211,4 +188,5 @@ module.exports = {
   ClassicValidator,
   SearchValidator,
   AddShortCommentValidator,
+  getUserListValidator,
 };
--- core/db.js
+++ core/db.js
@@ -1,50 +1,75 @@
-const {Sequelize,Model} =require('sequelize')
-const {unset,clone,isArray} =require('lodash')
-const {
-    dbName,
-    host,
-    port,
-    user,
-    password
-} = require('../config/config').database
+const { Sequelize, Model } = require("sequelize");
+const { unset, clone, isArray } = require("lodash");
+// const {
+//   dbName,
+//   host,
+//   port,
+//   user,
+//   password,
+// } = require("../config/config").database;
+let database;
+if (process.env.NODE_ENV === "production") {
+  console.log("production");
+  database = {
+    dbName: "arslan",
+    host: "119.45.0.151",
+    port: 3306,
+    user: "root",
+    password: "123456",
+  };
+}
 
-const sequelize = new Sequelize(dbName, user, password, {
-    dialect: 'mysql',
-    host: host,
-    port: port,
+if (process.env.NODE_ENV === "development") {
+  console.log("development");
+  database = {
+    dbName: "arslan",
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "159753",
+  };
+}
+const sequelize = new Sequelize(
+  database.dbName,
+  database.user,
+  database.password,
+  {
+    dialect: "mysql",
+    host: database.host,
+    port: database.port,
     logging: true,
-    timezone: '+08:00',
+    timezone: "+08:00",
     define: {
-        //create_time生成时间 update_time更新时间
-        timestamps: true,
-        paranoid: true,
-        createdAt: 'created_at',
-        updatedAt: 'updated_at',
-        deletedAt: 'deleted_at',
-        // 将自动设置所有属性的字段参数为下划线命名方式.
-        // 不会覆盖已经定义的字段选项
-        underscored: true,
-        
-    }
-})
+      //create_time生成时间 update_time更新时间
+      timestamps: true,
+      paranoid: true,
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+      deletedAt: "deleted_at",
+      // 将自动设置所有属性的字段参数为下划线命名方式.
+      // 不会覆盖已经定义的字段选项
+      underscored: true,
+    },
+  }
+);
 //自动化注册模型
-sequelize.sync()
+sequelize.sync();
 
-Model.prototype.toJSON =function(){
-    //浅拷贝
-    let data = clone(this.dataValues)
-    unset(data,'created_at')
-    unset(data,'deleted_at')
-    unset(data,'updated_at')
+Model.prototype.toJSON = function () {
+  //浅拷贝
+  let data = clone(this.dataValues);
+  unset(data, "created_at");
+  unset(data, "deleted_at");
+  unset(data, "updated_at");
 
-    if(isArray(this.exclude)){
-        this.exclude.forEach(item => {
-            unset(data,item)
-        });
-    }
+  if (isArray(this.exclude)) {
+    this.exclude.forEach((item) => {
+      unset(data, item);
+    });
+  }
 
-    return data
-}
+  return data;
+};
 module.exports = {
-    sequelize
-}
+  sequelize,
+};
